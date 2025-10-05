320e4e281d6a6de6c3113f4689fae39ae9c8f625
KaranChauhanKC <50906376+KaranChauhanKC@users.noreply.github.com>
2025-10-05 12:31:38 +0530

    Update Cyclist_Project.sql

--- Cyclist_Project.sql
+++ Cyclist_Project.sql
@@ -1,322 +1,242 @@
 USE cyclist;
 
-CREATE Table If NOT EXISTs 202507_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
+-- data was imported through import wizards. For files which were large and taking more time data was imported by load data infile function
 
-LOAD DATA INFILE '202507-divvy-tripdata.csv' INTO Table 202507_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-CREATE Table If NOT EXISTs 202506_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-CREATE Table If NOT EXISTs 202505_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-CREATE Table If NOT EXISTs 202504_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-CREATE Table If NOT EXISTs 202503_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-    
-CREATE Table If NOT EXISTs 202502_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-    
-CREATE Table If NOT EXISTs 202501_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-CREATE Table If NOT EXISTs 202412_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-CREATE Table If NOT EXISTs 202411_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-CREATE Table If NOT EXISTs 202410_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-    
-CREATE Table If NOT EXISTs 202409_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-    
-CREATE Table If NOT EXISTs 202408_tripdata
-(
-    rideable_type VARCHAR(255),
-    started_at_date DATE,
-    started_at_time TIME,
-    ended_at_date DATE,
-    ended_at_time TIME,
-    member_casual VARCHAR(255),
-    Weekday_ride VARCHAR(255),
-    Hour_ride int
-    );
-
-LOAD DATA INFILE '202506-divvy-tripdata.csv' INTO Table 202506_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202505-divvy-tripdata.csv' INTO Table 202505_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202504-divvy-tripdata.csv' INTO Table 202504_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202503-divvy-tripdata.csv' INTO Table 202503_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202502-divvy-tripdata.csv' INTO Table 202502_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202501-divvy-tripdata.csv' INTO Table 202501_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202412-divvy-tripdata.csv' INTO Table 202412_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202411-divvy-tripdata.csv' INTO Table 202411_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202410-divvy-tripdata.csv' INTO Table 202410_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202409-divvy-tripdata.csv' INTO Table 202409_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-LOAD DATA INFILE '202408-divvy-tripdata.csv' INTO Table 202408_tripdata
-FIELDS terminated by ','
-Ignore 1 Lines;
-
-CREATE Table IF NOT EXISTS 2425_tripdata_combined AS 
-(
-	Select * From 202408_tripdata
-    UNION ALL
-    Select * From 202409_tripdata
-    UNION ALL
-    Select * From 202410_tripdata
-    UNION ALL
-    Select * From 202411_tripdata
-    UNION ALL
-    Select * From 202412_tripdata
-    UNION ALL
-    Select * From 202501_tripdata
-    UNION ALL
-    Select * From 202502_tripdata
-    UNION ALL
-    Select * From 202503_tripdata
-    UNION ALL
-	Select * From 202504_tripdata
-    UNION ALL
-	Select * From 202505_tripdata
-    UNION ALL
-	Select * From 202506_tripdata
-	UNION ALL
-	Select * From 202507_tripdata
+CREATE TABLE IF NOT EXISTS `2022_tripdata.combined_data` AS (
+  SELECT * FROM `2022_tripdata.202201_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202202_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202203_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202204_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202205_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202206_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202207_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202208_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202209_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202210_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202211_tripdata`
+  UNION ALL
+  SELECT * FROM `2022_tripdata.202212_tripdata`
 );
 
--- checking no of rows which are 5611500
+-- checking no of rows which are 5667717
 
-Select Count(*) From 2425_tripdata_combined;
+SELECT COUNT(*)
+FROM `2022_tripdata.combined_data`;
+
+-- checking the data types of all columns
+
+SELECT column_name, data_type
+FROM `2022_tripdata`.INFORMATION_SCHEMA.COLUMNS
+WHERE table_name = 'combined_data';
 
 -- checking for number of null values in all columns
 
-SELECT
+SELECT COUNT(*) - COUNT(ride_id) ride_id,
  COUNT(*) - COUNT(rideable_type) rideable_type,
- COUNT(*) - COUNT(started_at_date) started_at_date,
- COUNT(*) - COUNT(started_at_time) started_at_time,
- COUNT(*) - COUNT(ended_at_date) ended_at_time,
- COUNT(*) - COUNT(member_casual) member_casual,
- COUNT(*) - COUNT(Weekday_ride) Weekday_ride,
- Count(*) - COUNT(Hour_ride) Hour_ride
-FROM 2425_tripdata_combined;
+ COUNT(*) - COUNT(started_at) started_at,
+ COUNT(*) - COUNT(ended_at) ended_at,
+ COUNT(*) - COUNT(start_station_name) start_station_name,
+ COUNT(*) - COUNT(start_station_id) start_station_id,
+ COUNT(*) - COUNT(end_station_name) end_station_name,
+ COUNT(*) - COUNT(end_station_id) end_station_id,
+ COUNT(*) - COUNT(start_lat) start_lat,
+ COUNT(*) - COUNT(start_lng) start_lng,
+ COUNT(*) - COUNT(end_lat) end_lat,
+ COUNT(*) - COUNT(end_lng) end_lng,
+ COUNT(*) - COUNT(member_casual) member_casual
+FROM `2022_tripdata.combined_data`;
 
-ALTER Table 2425_tripdata_combined
-Add column time_taken INT;
+-- checking for duplicate rows
 
-SET SQL_SAFE_UPDATES = 0;
+SELECT COUNT(ride_id) - COUNT(DISTINCT ride_id) AS duplicate_rows
+FROM `2022_tripdata.combined_data`;
 
-UPDATE 2425_tripdata_combined
-SET time_taken = TIMESTAMPDIFF(MINUTE, started_at_time, ended_at_time);
+-- ride_id - all have length of 16
 
-SET SQL_SAFE_UPDATES = 1;
+SELECT LENGTH(ride_id) AS length_ride_id, COUNT(ride_id) AS no_of_rows
+FROM `2022_tripdata.combined_data`
+GROUP BY length_ride_id;
 
-SET SQL_SAFE_UPDATES = 0;
+-- rideable_type - 3 unique types of bikes
 
-UPDATE 2425_tripdata_combined
-SET time_taken = time_taken + 1440
-Where time_taken < 0;
+SELECT DISTINCT rideable_type, COUNT(rideable_type) AS no_of_trips
+FROM `2022_tripdata.combined_data`
+GROUP BY rideable_type;
 
-SET SQL_SAFE_UPDATES = 1;
+-- started_at, ended_at - TIMESTAMP - YYYY-MM-DD hh:mm:ss UTC
 
-ALTER Table 2425_tripdata_combined
-Add column Month_ride VARCHAR(255);
+SELECT started_at, ended_at
+FROM `2022_tripdata.combined_data`
+LIMIT 10;
 
-SET SQL_SAFE_UPDATES = 0;
+SELECT COUNT(*) AS longer_than_a_day
+FROM `2022_tripdata.combined_data`
+WHERE (
+  EXTRACT(HOUR FROM (ended_at - started_at)) * 60 +
+  EXTRACT(MINUTE FROM (ended_at - started_at)) +
+  EXTRACT(SECOND FROM (ended_at - started_at)) / 60) >= 1440;   -- longer than a day - total rows = 5360
 
-UPDATE 2425_tripdata_combined
-SET Month_ride = monthname(started_at_date);
+SELECT COUNT(*) AS less_than_a_minute
+FROM `2022_tripdata.combined_data`
+WHERE (
+  EXTRACT(HOUR FROM (ended_at - started_at)) * 60 +
+  EXTRACT(MINUTE FROM (ended_at - started_at)) +
+  EXTRACT(SECOND FROM (ended_at - started_at)) / 60) <= 1;      -- less than a minute - total rows = 122283
 
-SET SQL_SAFE_UPDATES = 1;
+-- start_station_name, start_station_id - total 833064 rows with both start station name and id missing
 
-SET SQL_SAFE_UPDATES = 0;
+SELECT DISTINCT start_station_name
+FROM `2022_tripdata.combined_data`
+ORDER BY start_station_name;
 
-DELETE FROM 2425_tripdata_combined
-WHERE time_taken <=1;
+SELECT COUNT(ride_id) AS rows_with_start_station_null          -- return 833064 rows
+FROM `2022_tripdata.combined_data`
+WHERE start_station_name IS NULL OR start_station_id IS NULL;
 
-SET SQL_SAFE_UPDATES = 1;
+-- end_station_name, end_station_id - total 892742 rows with both end station name and id missing
 
--- SELECT * FROM 2425_tripdata_combined
--- INTO OUTFILE 'C:\Users\1996k\Downloads\case study 1\Cyclist\2425_tripdata_combined.csv'
--- FIELDS TERMINATED BY ',' 
--- LINES TERMINATED BY '\n';
+SELECT DISTINCT end_station_name
+FROM `2022_tripdata.combined_data`
+ORDER BY end_station_name;
 
--- bikes types used by rider types
+SELECT COUNT(ride_id) AS rows_with_null_end_station          -- return 892742 rows
+FROM `2022_tripdata.combined_data`
+WHERE end_station_name IS NULL OR end_station_id IS NULL;
+
+-- end_lat, end_lng - total 5858 rows with both missing
+
+SELECT COUNT(ride_id) AS rows_with_null_end_loc
+FROM `2022_tripdata.combined_data`
+WHERE end_lat IS NULL OR end_lng IS NULL;
+
+-- member_casual - 2 unique values - member and casual riders
+
+SELECT DISTINCT member_casual, COUNT(member_casual) AS no_of_trips
+FROM `2022_tripdata.combined_data`
+GROUP BY member_casual;
+
+-- creating new table with clean data
+
+CREATE TABLE IF NOT EXISTS `2022_tripdata.cleaned_combined_data` AS (
+  SELECT 
+    a.ride_id, rideable_type, started_at, ended_at, 
+    ride_length,
+    CASE EXTRACT(DAYOFWEEK FROM started_at) 
+      WHEN 1 THEN 'SUN'
+      WHEN 2 THEN 'MON'
+      WHEN 3 THEN 'TUES'
+      WHEN 4 THEN 'WED'
+      WHEN 5 THEN 'THURS'
+      WHEN 6 THEN 'FRI'
+      WHEN 7 THEN 'SAT'    
+    END AS day_of_week,
+    CASE EXTRACT(MONTH FROM started_at)
+      WHEN 1 THEN 'JAN'
+      WHEN 2 THEN 'FEB'
+      WHEN 3 THEN 'MAR'
+      WHEN 4 THEN 'APR'
+      WHEN 5 THEN 'MAY'
+      WHEN 6 THEN 'JUN'
+      WHEN 7 THEN 'JUL'
+      WHEN 8 THEN 'AUG'
+      WHEN 9 THEN 'SEP'
+      WHEN 10 THEN 'OCT'
+      WHEN 11 THEN 'NOV'
+      WHEN 12 THEN 'DEC'
+    END AS month,
+    start_station_name, end_station_name, 
+    start_lat, start_lng, end_lat, end_lng, member_casual
+  FROM `2022_tripdata.combined_data` a
+  JOIN (
+    SELECT ride_id, (
+      EXTRACT(HOUR FROM (ended_at - started_at)) * 60 +
+      EXTRACT(MINUTE FROM (ended_at - started_at)) +
+      EXTRACT(SECOND FROM (ended_at - started_at)) / 60) AS ride_length
+    FROM `2022_tripdata.combined_data`
+  ) b 
+  ON a.ride_id = b.ride_id
+  WHERE 
+    start_station_name IS NOT NULL AND
+    end_station_name IS NOT NULL AND
+    end_lat IS NOT NULL AND
+    end_lng IS NOT NULL AND
+    ride_length > 1 AND ride_length < 1440
+);
+
+ALTER TABLE `2022_tripdata.cleaned_combined_data`     -- set ride_id as primary key
+ADD PRIMARY KEY(ride_id) NOT ENFORCED;
+
+SELECT COUNT(ride_id) AS no_of_rows       -- returned 4,291,805 rows so 1,375,912 rows removed
+FROM `2022_tripdata.cleaned_combined_data`;
+
+-- bikes types used by riders
 
 SELECT member_casual, rideable_type, COUNT(*) AS total_trips
-FROM 2425_tripdata_combined
+FROM `2022_tripdata.cleaned_combined_data`
 GROUP BY member_casual, rideable_type
 ORDER BY member_casual, total_trips;
 
 -- no. of trips per month
 
-SELECT Month_ride, member_casual, COUNT(*) AS total_trips
-FROM 2425_tripdata_combined
-GROUP BY Month_ride, member_casual
+SELECT month, member_casual, COUNT(ride_id) AS total_trips
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY month, member_casual
 ORDER BY member_casual;
 
--- no. of trips per week
+-- no. of trips per day of week
 
-SELECT Weekday_ride, member_casual, COUNT(*) AS total_trips
-FROM 2425_tripdata_combined
-GROUP BY Weekday_ride, member_casual
+SELECT day_of_week, member_casual, COUNT(ride_id) AS total_trips
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY day_of_week, member_casual
 ORDER BY member_casual;
 
 -- no. of trips per hour
 
-SELECT Hour_ride, member_casual, COUNT(*) AS total_trips
-FROM 2425_tripdata_combined
-GROUP BY Hour_ride, member_casual
+SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, COUNT(ride_id) AS total_trips
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY hour_of_day, member_casual
 ORDER BY member_casual;
 
 -- average ride_length per month
 
-SELECT Month_ride, member_casual, AVG(time_taken) AS avg_ride_duration
-FROM 2425_tripdata_combined
-GROUP BY Month_ride, member_casual;
+SELECT month, member_casual, AVG(ride_length) AS avg_ride_duration
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY month, member_casual;
 
 -- average ride_length per day of week
 
-SELECT Weekday_ride, member_casual, AVG(time_taken) AS avg_ride_duration
-FROM 2425_tripdata_combined
-GROUP BY Weekday_ride, member_casual;
+SELECT day_of_week, member_casual, AVG(ride_length) AS avg_ride_duration
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY day_of_week, member_casual;
 
 -- average ride_length per hour
 
-SELECT Hour_ride, member_casual, AVG(time_taken) AS avg_ride_duration
-FROM 2425_tripdata_combined
-GROUP BY Hour_ride, member_casual;
+SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, AVG(ride_length) AS avg_ride_duration
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY hour_of_day, member_casual;
+
+-- starting station locations
+
+SELECT start_station_name, member_casual,
+  AVG(start_lat) AS start_lat, AVG(start_lng) AS start_lng,
+  COUNT(ride_id) AS total_trips
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY start_station_name, member_casual;
+
+-- ending station locations
+
+SELECT end_station_name, member_casual,
+  AVG(end_lat) AS end_lat, AVG(end_lng) AS end_lng,
+  COUNT(ride_id) AS total_trips
+FROM `2022_tripdata.cleaned_combined_data`
+GROUP BY end_station_name, member_casual;
